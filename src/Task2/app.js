d1772e3e523677d32a8f786bcaa7be518405868a
Cesar Ramirez <cramirez@corp.na.greensky.net>
2020-08-04 21:06:18 -0500

    File Structure refactor

--- src/Task2/app.js
+++ src/Task2/app.js
@@ -1,8 +1,8 @@
 const csv = require("csvtojson");
 const fs = require("fs");
-const writer = fs.createWriteStream("./src/Task2/output.txt");
+const writer = fs.createWriteStream("./data/outputFiles/output.txt");
 
-fs.createReadStream("./src/Task2/csv/nodejs-hw1-ex1.csv")
+fs.createReadStream("./data/csv/nodejs-hw1-ex1.csv")
   .pipe(csv())
   .on("data", function (row) {
     writer.write(row);
