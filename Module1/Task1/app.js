31d52d6cebb7c56cb6963fb5066d98c5479aebd2
Cesar Ramirez <cramirez@corp.na.greensky.net>
2020-08-04 20:31:38 -0500

    Renamed src folder

--- Module1/Task1/app.js
+++
@@ -1,7 +0,0 @@
-const process = require("process");
-const { stdin, stdout } = process;
-
-stdin.addListener("data", (input) => {
-  const reversedInput = input.reverse().toString().trim();
-  stdout.write(`${reversedInput} \n`);
-});
--- src/Task2/app.js
+++ src/Task2/app.js
@@ -1,8 +1,8 @@
 const csv = require("csvtojson");
 const fs = require("fs");
-const writer = fs.createWriteStream("./Module1/Task2/output.txt");
+const writer = fs.createWriteStream("./src/Task2/output.txt");
 
-fs.createReadStream("./Module1/Task2/csv/nodejs-hw1-ex1.csv")
+fs.createReadStream("./src/Task2/csv/nodejs-hw1-ex1.csv")
   .pipe(csv())
   .on("data", function (row) {
     writer.write(row);
